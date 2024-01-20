efcd1490476345087b0fec91f50e8ed7ed6e4fb9
PawanSirsat <p1.sirsat1998@gmail.com>
2024-01-21 00:41:21 +0530

    Commit

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,14 +1,14 @@
 import { Client, Account, Databases, Storage, Avatars } from "appwrite";
 
 export const appwriteConfig = {
-  url: "https://cloud.appwrite.io/v1", // Updated URL value
-  projectId: "657c067c93411fbcf173", // Updated project ID value
-  databaseId: "657c0953b37f27853da8", // Updated database ID value
-  storageId: "655731fab3467412e016", // Updated storage ID value
-  userCollectionId: "657c096db7f49cee3b20", // Updated user collection ID value
-  groupsCollectionId: "657c098394249f087496", // Updated post collection ID value
-  activityCollectionId: "657c099dd2eddbdd9ebb", // Updated saves collection ID value
-  friendsCollectionId: "6581b28b356ccbedd28d", // Updated saves collection ID value
+  url: "https://cloud.appwrite.io/v1",
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+  userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+  groupsCollectionId: import.meta.env.VITE_APPWRITE_GROUPS_COLLECTION_ID,
+  activityCollectionId: import.meta.env.VITE_APPWRITE_ACTIVITY_COLLECTION_ID,
+  friendsCollectionId: import.meta.env.VITE_APPWRITE_FRIENDS_COLLECTION_ID,
 };
 
 export const client = new Client();
